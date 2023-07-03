ad8f43e7a540250520ddd17d53a8783b5b9e9911
junhIT <tjsej1324@gmail.com>
2023-07-04 00:01:21 +0900

    섹션4.회원리포지토리개발

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -1,23 +1,35 @@
 package jpabook.jpashop;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
 
-@Repository
+@Repository	// SpringBean 등록
 public class MemberRepository {
 
 	@PersistenceContext
 	private EntityManager em;
 	
-	public Long save(Member member) {
+	public void save(Member member) {
 		em.persist(member);
-		return member.getId();
 	}
 	
-	public Member find(Long id) {
+	public Member findOne(Long id) {
 		return em.find(Member.class, id);
+	}
+	
+	public List<Member> findAll() {
+		return em.createQuery("select m from Member m", Member.class)
+				.getResultList();
+	}
+	
+	public List<Member> findByNames(String name) {
+		return em.createQuery("select m from Member where m.name = :name", Member.class)
+				.setParameter("name", name)
+				.getResultList();
 	}
 }
