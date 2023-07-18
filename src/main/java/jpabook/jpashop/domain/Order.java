efc193bbd2be40b87a11235ee49a43e9fd33c623
junhIT <tjsej1324@gmail.com>
2023-07-18 23:10:05 +0900

    섹선6 주문,주문상품엔티티개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,8 +4,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -64,5 +62,48 @@ public class Order {
     public void setDelivery(Delivery delivery) {
     	this.delivery = delivery;
     	delivery.setOrder(this);
+    }
+    
+    //== 생성 메서드 ==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+    	Order order = new Order();
+    	order.setMember(member);
+    	order.setDelivery(delivery);
+    	for (OrderItem orderItem : orderItems) {
+    		order.addOrderItem(orderItem);
+    	}
+    	
+    	order.setStatus(OrderStatus.ORDER);
+    	order.setOrderDate(LocalDateTime.now());
+    	
+		return order;
+    }
+    
+    //== 비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+    	if (delivery.getStatus() == DeliveryStatus.COMP) {
+    		throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+    	}
+    	
+    	this.setStatus(OrderStatus.CANCEL);
+    	for (OrderItem orderItem : orderItems) {
+    		orderItem.cancel();
+    	}
+    }
+    
+    //== 조회 로직 ==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+    	int totalPrice = 0;
+    	for (OrderItem orderItem : orderItems) {
+    		totalPrice += orderItem.getTotalPrice();
+    	}
+    	
+    	return totalPrice;
     }
 }
